4711bfe96af0ad6be5c3dd54d9a16569c9d8424b
Ivan Kozik <ivan@ludios.org>
2023-08-30 05:02:13 +0000

    Nick coloring algorithm: remove the leading "M-" from the Matrix bridge users

--- client/js/libs/handlebars/colorClass.js
+++ client/js/libs/handlebars/colorClass.js
@@ -3,8 +3,11 @@
 // Use xchat's coloring algorithm; see xchat/src/common/text.c
 const rcolors = [19, 20, 22, 24, 25, 26, 27, 28, 29];
 
-// Generates a string from "color-1" to "color-32" based on an input string
+// Generates a string from "color-19" to "color-29" based on an input string
 module.exports = function(str) {
+	// Matrix Bridge People
+	str = str.replace(/^M-/, "");
+
 	let sum = 0;
 
 	for (let i = 0; i < str.length; i++) {
