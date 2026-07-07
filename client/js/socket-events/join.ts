1d8dfdd95eb3774aa1cbd838152016e0ae0bc1c2
ludios <ludios@users.noreply.github.com>
2026-07-07 08:18:40 +0000

    Don't automatically focus newly joined channels
    Port of 8e2e6d5e: joining (including whois-opened queries) only adds the
    channel to the sidebar; the current view stays put.

--- client/js/socket-events/join.ts
+++ client/js/socket-events/join.ts
@@ -1,6 +1,5 @@
 import socket from "../socket";
 import {store} from "../store";
-import {switchToChannel} from "../router";
 import {ClientChan} from "../types";
 import {toClientChan} from "../chan";
 
@@ -13,18 +12,4 @@ socket.on("join", function (data) {
 
 	const clientChan: ClientChan = toClientChan(data.chan);
 	network.channels.splice(data.index || -1, 0, clientChan);
-
-	// Queries do not automatically focus, unless the user did a whois
-	if (data.chan.type === "query" && !data.shouldOpen) {
-		return;
-	}
-
-	const chan = store.getters.findChannel(data.chan.id);
-
-	if (chan) {
-		switchToChannel(chan.channel);
-	} else {
-		// eslint-disable-next-line no-console
-		console.error("Could not find channel", data.chan.id);
-	}
 });
